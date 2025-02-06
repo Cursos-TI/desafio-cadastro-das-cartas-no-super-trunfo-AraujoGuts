1061325c8d3ac0cb33a84d64c678e5a1c5240a4e
AraujoGuts <gustavoctt2024@gmail.com>
2025-02-06 19:34:00 +0000

    Atualizaçao com operaçoes relacionamentais e novos valores!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 int main(){
      ///Variaveis:
     char Cidade[20], Estado[20], Codigo[20], Pais[20];
-    float Populacao, PIB, Area;
+    float Populacao, PIB, Area, Densidadepopulacional, PIBPerCapita;
     int Pontosturisticos;
  
  /// Titulo do Jogo:
@@ -36,6 +36,9 @@ int main(){
   printf("Quantidade de Pontos Turisticos: \n");
   scanf("%d", &Pontosturisticos); 
   
+  Densidadepopulacional = (Populacao / Area);
+  PIBPerCapita = (PIB / Populacao);
+
   ///Abertura da apresentaçao dos dados da carta:
 
   printf("\n***Sua carta é:***\n\n");
@@ -45,6 +48,9 @@ int main(){
 
   printf("País: %s \n Estado: %s \n Cidade: %s \n Codigo: %s \n", Pais, Estado, Cidade, Codigo);
   printf("População: %f \n PIB: %f \n Área: %f \n Pontos Turisticos: %d \n", Populacao, PIB, Area, Pontosturisticos);
+  printf("Densidade Populacional: %.3f \n PIB Per Capita:%.3f \n ", Densidadepopulacional, PIBPerCapita);
+  
+
 
 
     return 0;
